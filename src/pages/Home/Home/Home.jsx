641b67549eea715db0ea87192323dc431f58618c
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-10 18:44:21 +0600

    swipper.js added for home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../../shared/NavBar/Banner/Banner"
+import Category from "../Category/Category"
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     )
 }
