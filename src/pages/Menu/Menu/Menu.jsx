0b9db580d3739d3b544cbace8b3ea33ba042edd6
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-15 23:51:38 +0600

    error fixed

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -2,8 +2,14 @@ import { Helmet } from "react-helmet-async"
 import Cover from "../../../shared/Cover/Cover"
 import menuImg from '../../../assets/menu/banner3.jpg'
 import PopularMenu from "../../Home/PopularMenu/PopularMenu"
+import useMenu from "../../hooks/useMenu"
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const dessert = menu.filter(item => item.category === 'dessert')
+  const soup = menu.filter(item => item.category === 'soup')
+  const salad = menu.filter(item => item.category === 'salad')
+  const pizza = menu.filter(item => item.category === 'pizza')
     return (
         <div>
           <Helmet>
