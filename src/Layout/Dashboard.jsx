f8e36fbdefdda6ef1e4d3478a33f5772bf8e3aad
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-21 01:30:44 +0600

    Dashboard cart added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,11 @@
 import { FaAd, FaHome, FaShoppingCart } from "react-icons/fa";
 import { FaCalendar, FaList } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom"
+import useCart from "../pages/hooks/useCart";
 
 
 const Dashboard = () => {
+    const [cart] = useCart()
     return (
         <div className="flex ">
             {/* Dashboard sidebar */}
@@ -13,7 +15,7 @@ const Dashboard = () => {
                         <NavLink to={'/dashboard/userHome'}><FaHome />User Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to={'/dashboard/cart'}><FaShoppingCart />My Cart</NavLink>
+                        <NavLink to={'/dashboard/cart'}><FaShoppingCart />My Cart ({cart.length})</NavLink>
                     </li>
                     <li>
                         <NavLink to={'/dashboard/reservation'}><FaCalendar />Severvation</NavLink>
@@ -24,10 +26,17 @@ const Dashboard = () => {
                     <li>
                         <NavLink to={'/dashboard/bookings'}><FaList />My Bookings</NavLink>
                     </li>
+                    <div className="divider"></div>
+                    <li>
+                        <NavLink to={'/'}><FaList />Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/menu'}><FaList />menu</NavLink>
+                    </li>
                 </ul>
             </div>
             {/* dashboard content */}
-            <div className="flex-1">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
