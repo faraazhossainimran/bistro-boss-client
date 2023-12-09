a397dcf031377aa2653140313779a5f2db9370ba
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-12-09 11:13:09 +0600

    link added

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
 https://drive.google.com/drive/folders/1iQqmdaywAyVuzdF37Azl5auftBEYhn9e
+https://drive.google.com/file/d/1IOesOzFrvGsoBPqqg0wT64eipYE7yA41/view?usp=sharing
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -12,6 +12,7 @@ import Cart from "../pages/Dashboard/Cart/Cart";
 import AllUsers from "../pages/Dashboard/AllUsers/AllUsers";
 import AddItems from "../pages/AddItems/AddItems";
 import AdminRoute from "./AdminRoute";
+import ManageItems from "../pages/Dashboard/ManageItems/ManageItems";
 
 export const router = createBrowserRouter([
   {
@@ -50,22 +51,42 @@ export const router = createBrowserRouter([
   },
   {
     path: "dashboard",
-    element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
+    element: (
+      <PrivateRoute>
+        <Dashboard></Dashboard>
+      </PrivateRoute>
+    ),
     children: [
       // normal user route
       {
-          path: "cart",
-          element: <Cart></Cart>
+        path: "cart",
+        element: <Cart></Cart>,
       },
       // admin routes
       {
-        path: 'addItems',
-        element: <AdminRoute><AddItems></AddItems></AdminRoute>
+        path: "addItems",
+        element: (
+          <AdminRoute>
+            <AddItems></AddItems>
+          </AdminRoute>
+        ),
       },
       {
         path: "users",
-        element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
-    }
+        element: (
+          <AdminRoute>
+            <AllUsers></AllUsers>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manageItems",
+        element: (
+          <AdminRoute>
+            <ManageItems></ManageItems>
+          </AdminRoute>
+        ),
+      },
     ],
   },
 ]);
